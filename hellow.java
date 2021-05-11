c761e69a275d8e61d3309aee7c78deddc5cb4e36
Aman Verma <amanmehta2007@gmail.com>
2021-05-11 18:22:50 +0000

    gitpod-bramch

---
+++ hellow.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,Aman!"); 
+        Scanner obj = new Scanner(System.in);
+        int i = obj.nextInt();
+        System.out.println(i);
+        //world.fun();
+        
+    obj.close();
+    }
+}
